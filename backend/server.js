04eaf3ff9d31c8a4dc70f9e6fd1b6b7b9742ddbe
Satyam-git1201 <kumarsatyam14801@gmail.com>
2021-06-22 14:01:48 +0530

    error handling through middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -10,11 +11,20 @@ connectDB()
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log(req.originalUrl)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(

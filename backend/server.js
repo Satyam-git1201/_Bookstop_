b56dde33a62ffb778881c520c5ef366d592d89ff
Satyam-git1201 <kumarsatyam14801@gmail.com>
2021-06-26 09:17:56 +0530

    created userRoutes and userController for authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -10,6 +11,8 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.originalUrl)
@@ -21,10 +24,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
-app.use(notFound)
-
-app.use(errorHandler)
+app.use(notFound), app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(

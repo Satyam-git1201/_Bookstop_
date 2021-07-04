26df3eda1bcc3711705be238d63664f85e5c01a3
Satyam-git1201 <kumarsatyam14801@gmail.com>
2021-07-04 15:46:04 +0530

    added place order screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -46,7 +46,10 @@ export const cartReducer = (
       }
 
     case USER_LOGOUT:
-      return {}
+      return {
+        cartItems: [],
+        shippingAddress: {},
+      }
 
     default:
       return state
---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react'
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart)
+
+  //Calculate prices:
+  const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+  }
+
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  )
+
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 500 ? 0 : 80)
+
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
+  const placeOrderHandler = () => {
+    console.log('Order')
+  }
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flux'>
+            <ListGroup.Item className='my-2 py-2'>
+              <h3 className='plorder my-2'>SHIPPING</h3>
+              <p style={{ fontFamily: 'revert', color: 'rgb(171, 187, 220)' }}>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{' '}
+                {cart.shippingAddress.postalCode},{' '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item className='my-2 py-2'>
+              <h3 className='plorder my-2'>PAYMENT METHOD</h3>
+              <p style={{ fontFamily: 'revert', color: 'rgb(171, 187, 220)' }}>
+                <strong>Method: </strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item className='py-2 my-2'>
+              <h3 className='plorder my-2'>ORDER ITEMS</h3>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty!</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link
+                            to={`/product/${item.product}`}
+                            style={{ textDecoration: 'none' }}
+                            className='linklink'
+                          >
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ₹{item.price} = ₹{item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3 className='plorder'>ORDER SUMMARY</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>₹{cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>₹{cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>₹{cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>₹{cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type='button'
+                  className='w-100'
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default PlaceOrderScreen

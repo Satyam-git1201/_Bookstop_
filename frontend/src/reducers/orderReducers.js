69d65e71750a9631774598a1ce844c7b2f9ca162
Satyam-git1201 <kumarsatyam14801@gmail.com>
2021-07-06 10:37:45 +0530

    added order details reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants'
 import { USER_LOGOUT } from '../constants/userConstants'
 import { PRODUCT_LIST_REQUEST } from '../constants/productConstants'
@@ -20,6 +23,37 @@ export const orderCreateReducer = (state = {}, action) => {
         order: action.payload,
       }
     case ORDER_CREATE_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    case USER_LOGOUT:
+      return {}
+    case PRODUCT_LIST_REQUEST:
+      return {}
+    case CART_ADD_ITEM:
+      return {}
+    default:
+      return state
+  }
+}
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      }
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload,
+      }
+    case ORDER_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
